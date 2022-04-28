3ea299c1a931231c4ec4a008542cfa07dbd8303d
FigasaCHAN <acostafa.dev@gmail.com>
2022-04-28 02:29:42 -0300

    first commit

---
+++ src/main/java/com/acostafa/kruger/inventariovacuna/InventarioVacunaApplication.java
@@ -0,0 +1,13 @@
+package com.acostafa.kruger.inventariovacuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioVacunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioVacunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acostafa/kruger/inventariovacuna/InventarioVacunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acostafa.kruger.inventariovacuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioVacunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
