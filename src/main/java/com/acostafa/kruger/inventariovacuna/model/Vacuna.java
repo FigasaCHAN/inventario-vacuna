c959466ec19f9f82f8d44803176e96dd6cdc1af8
FigasaCHAN <acostafa.dev@gmail.com>
2022-04-29 13:16:44 -0300

    Add: Se agregan validaciones a la entidad Vacuna

--- src/main/java/com/acostafa/kruger/inventariovacuna/model/Vacuna.java
+++ src/main/java/com/acostafa/kruger/inventariovacuna/model/Vacuna.java
@@ -6,27 +6,45 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Table (name = "vacunas")
 public class Vacuna {
+
     @Id
     @Column(name = "id_vacuna")
-    private String id;
+    @NotBlank
+    @Length(min = 10, max = 10)
+    private String id; //este id estara relacionado al id del usuario
+
+    @NotBlank
+    @Length(max = 32)
+    @Pattern(regexp = "(^Sputnik$)|(^AstraZeneca$)|(^Pfizer$)|(^Jhonson&Jhonson$)", message = "Tipo no valido")
     private String tipo;
+
     private Date fechaDeAplicacion;
+    @Range(min = 0)
     private int numeroDeDosis;
     
     // region Constructores
     public Vacuna(){
 
     }
-    public Vacuna(String id, String tipo, Date fechaDeAplicacion, int numeroDeDosis) {
+    
+    public Vacuna(@NotBlank @Length(min = 10, max = 10) String id,
+            @NotBlank @Pattern(regexp = "(^Sputnik$)|(^AstraZeneca$)|(^Pfizer$)|(^Jhonson&Jhonson$)", message = "Tipo no valido") String tipo,
+            Date fechaDeAplicacion, @Range(min = 0) int numeroDeDosis) {
         this.id = id;
         this.tipo = tipo;
         this.fechaDeAplicacion = fechaDeAplicacion;
         this.numeroDeDosis = numeroDeDosis;
     }
+
     //endregion
     // region Getters y Setters
     public String getId() {
